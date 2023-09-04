7d27da5625fd049c53ae9c8f5578af7b23922ab5
isaquefreitasdev <freitasmelo.mkt.online@gmail.com>
2023-09-04 10:38:17 -0300

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Repositório de estudos sobre PHP
+_Você pode contribuir se achar necessário_
+```
+Aprender é melhor não aprender
+```
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <input type="text" name="user" id="">
+        <input type="text" name="id" id="">
+        <input type="submit" value="Enviar">
+    </form>
+    
+</body>
+</html>
+
+
+
+<?php
+  echo $_POST['user']
+?>
